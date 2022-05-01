0f07378dd040e19d1cf1269675e38a5cef743d0d
mbrecht <brecht.michael.b@gmail.com>
2022-04-30 18:31:38 -0600

    feat: creates meal-planner page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function Home() {
   return (
     <div>
       <h1>Just Cook</h1>
+      <Link href="/meal-planner">Meal Planner</Link>
     </div>
   );
 }
---
+++ src/pages/meal-planner.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MealPlanner() {
+  return (
+    <div>
+      <h1>Meal Planner</h1>
+    </div>
+  );
+}
