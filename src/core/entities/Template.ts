371b4fb09f8ed4ee718f459771831dc973a10462
mbrecht <brecht.michael.b@gmail.com>
2022-05-22 09:43:47 -0600

    chore: update documentation

--- src/core/entities/Template.ts
+++ src/core/entities/Template.ts
@@ -1,9 +1,12 @@
 export type category = string;
 
+/**
+ * A Day is an array of meal categories.
+ */
 export type Day = Array<category>;
 
 /**
- * A template is an array of meal options. Each
+ * A template is an array of Days which represent categories.
  */
 type Template = Array<Day>;
 
