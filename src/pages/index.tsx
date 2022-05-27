9b2b2de94ab495c17195bdb4874302563ffdf8f6
mbrecht <brecht.michael.b@gmail.com>
2022-05-27 15:10:40 -0600

    feat: adds recipe cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import RecipeCard from '../components/RecipeCard';
 import Link from 'next/link';
 import React from 'react';
 
@@ -6,6 +7,11 @@ export default function Home() {
     <div>
       <h1>Just Cook</h1>
       <Link href="/meal-planner">Meal Planner</Link>
+      <RecipeCard
+        title="test"
+        thumbnail="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2008/10/21/0/FNMChineseDumplingSoup_s3x4.jpg.rend.hgtvcom.826.620.suffix/1383173432945.jpeg"
+        url="https://www.google.com/"
+      />
     </div>
   );
 }
