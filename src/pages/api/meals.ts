120cf8951cd6d609598f6f7afe45638a4714d9c1
mbrecht <brecht.michael.b@gmail.com>
2022-06-02 09:45:53 -0600

    chore: add cors to route

--- src/pages/api/meals.ts
+++ src/pages/api/meals.ts
@@ -1,6 +1,20 @@
 import defaultHandler from '../../api/defaultHandler';
 import GET from '../../api/meals/get';
+import Cors from 'cors';
 import {NextApiRequest, NextApiResponse} from 'next';
+
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+});
+
+function applyMiddleware(req, res, cb) {
+  return new Promise((resolve, reject) => {
+    cb(req, res, result => {
+      if (result instanceof Error) return reject(result);
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,6 +24,8 @@ export default async function handler(
     GET,
     default: defaultHandler,
   };
+
+  await applyMiddleware(req, res, cors);
 
   const callback = handlers[req.method] || handlers.default;
 
